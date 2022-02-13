211b30781332b64e09b9ab0c0718799a6ced6103
Hongyu.Fan <hongyu.fan@sdqtech.com>
2022-02-13 16:55:30 +0800

    本地代码更新

--- src/pages/HomeGateway/index.tsx
+++ src/pages/HomeGateway/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Spin } from '@arco-design/web-react';
+import style from './index.module.less';
+
 
 export default function index() {
+
+  const Loading = () => {
+    return (
+      <div className={style.loadingContainer}>
+        <Spin tip='正在扫描当前局域网中的设备，请稍后...' loading dot />
+      </div>
+    )
+  }
+
   return (
-    <div>
-      HomeGateway
+    <div className={style.container}>
+      <Loading />
     </div>
   )
 }
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { Message } from '@arco-design/web-react';
-import router from '../history';
+import history from '../history';
 // 全局拦截器，这个文件不要动！！！！
 
 // 创建axios实例
 const service = axios.create({
   // baseURL: 'http://49.234.235.135:6060', // 服务器-前端解决跨域
   baseURL: 'http://localhost:6060', // 本地-前端解决跨域
-  timeout: 10000 // 请求超时时间
+  timeout: 6000 // 请求超时时间
 });
 
 // 配置全局的拦截器
@@ -30,21 +30,28 @@ service.interceptors.response.use(response => {
   return response.data;
 }, error => {
   // 当返回信息为未登录或者登录失效的时候重定向为登录页面
-  const { status = '400' } = error?.response
+
+  if (error?.message === 'Network Error') {
+    Message.error("网络错误，请稍后重试！");
+    return Promise.reject(error)
+  }
   // eslint-disable-next-line no-console
-  console.log(error)
+  console.log(history);
+  const { status = '400' } = error?.response
   switch (status) {
     case 400:
       Message.error("参数信息有误");
-      router.push("/login");
+      history.createHref("/login");
       break;
     case 401:
       Message.error("用户登录令牌过期，请重新登录");
-      router.push("/login");
+
+
+      history.createHref("/login");
       break;
     case 302:
       Message.error("用户未登录");
-      router.push("/login");
+      history.createHref("/login");
       break;
     case 403:
       Message.error('数据请求失败，请稍后重试！');
